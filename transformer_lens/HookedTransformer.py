f9374299c87777c36fe1af8b351308337540b188
Alain <alain.lenoach29170@gmail.com>
2023-07-07 18:19:36 +0200

    Support for reduced precision (#104)

--- transformer_lens/HookedTransformer.py
+++ transformer_lens/HookedTransformer.py
@@ -838,6 +838,10 @@ class HookedTransformer(HookedRootModule):
                 functions when compatible. For some models or arguments it doesn't work, especially for
                 models that are not internally loaded with HuggingFace's from_pretrained (e.g. SoLU models).
         """
+        assert not (
+            from_pretrained_kwargs.get("load_in_8bit", False)
+            or from_pretrained_kwargs.get("load_in_4bit", False)
+        ), "Quantization not supported"
 
         # Get the model name used in HuggingFace, rather than the alias.
         official_model_name = loading.get_official_model_name(model_name)
@@ -883,10 +887,6 @@ class HookedTransformer(HookedRootModule):
 
         # Create the HookedTransformer object
         model = cls(cfg, tokenizer, move_to_device)
-
-        dtype = from_pretrained_kwargs.get("torch_dtype", None)
-        if dtype is not None:
-            model = model.to(dtype)
 
         model.load_and_process_state_dict(
             state_dict,
@@ -996,6 +996,7 @@ class HookedTransformer(HookedRootModule):
 
         if move_state_dict_to_device:
             for k, v in state_dict.items():
+                v = v.to(self.cfg.dtype)
                 if k.startswith("embed") or k.startswith("pos_embed"):
                     state_dict[k] = v.to(
                         devices.get_device_for_block_index(0, self.cfg)
@@ -1400,7 +1401,7 @@ class HookedTransformer(HookedRootModule):
         prepend_bos=True,
         return_type: Optional[str] = "input",
         verbose: bool = True,
-    ) -> Float[torch.Tensor, "batch pos_plus_new_tokens"]:
+    ) -> Union[Int[torch.Tensor, "batch pos_plus_new_tokens"], str]:
         """
         Sample tokens from the model until the model outputs eos_token or max_new_tokens is reached.
 
